8a5eddff24d554d60612e7e6aa0a3e86c8488057
kubavation <kubavation@wp.pl>
2019-10-21 22:05:18 +0200

    addPost action

---
+++ ngrx-project/src/app/actions/post.actions.ts
@@ -0,0 +1,7 @@
+import { PostActionTypes } from './../shared/enum/PostActionTypes.enum';
+import { ActionParent } from './action.parent';
+
+export class AddPost implements ActionParent {
+    type: PostActionTypes.ADD;
+    constructor(public payload: any) {}
+}
