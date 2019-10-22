9d467c198123c4c1e815ecc306299140169051a6
kubavation <kubavation@wp.pl>
2019-10-22 16:02:34 +0200

    remove and update post actions

--- ngrx-project/src/app/actions/post.actions.ts
+++ ngrx-project/src/app/actions/post.actions.ts
@@ -1,3 +1,4 @@
+import { Action } from '@ngrx/store';
 import { Post } from './../model/Post';
 import { PostActionTypes } from './../shared/enum/PostActionTypes.enum';
 import { ActionParent } from './action.parent';
@@ -10,5 +11,15 @@ export class AddPost implements ActionParent {
 export class FetchPosts implements ActionParent {
     type = PostActionTypes.FETCH;
     constructor(public payload: string) {}
+}
+
+export class RemovePost implements ActionParent {
+    type = PostActionTypes.REMOVE;
+    constructor(public payload: number) {}
+}
+
+export class UpdatePost implements ActionParent {
+    type = PostActionTypes.UPDATE;
+    constructor(public payload: Post) {}
 }
 //todo
