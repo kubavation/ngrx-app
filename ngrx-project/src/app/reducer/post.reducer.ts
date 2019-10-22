ba8dd04535496c12983eb63271e8d5a9ef0e287a
kubavation <kubavation@wp.pl>
2019-10-22 17:16:38 +0200

    fix in postReducer

--- ngrx-project/src/app/reducer/post.reducer.ts
+++ ngrx-project/src/app/reducer/post.reducer.ts
@@ -1,18 +1,30 @@
+import { State } from './state';
 import { AddPost } from './../actions/post.actions';
 import { ActionParent } from './../actions/action.parent';
 import { PostActionTypes } from './../shared/enum/PostActionTypes.enum';
 import { Post } from './../model/Post';
 
-export const initialState: Post[] = [];
+export const initialState: State = {
+    posts: []
+};
 
 export function PostReducer(state = initialState, action: ActionParent) {
     switch (action.type) {
         case PostActionTypes.ADD:
-            return [...state, action.payload];
+            return {
+                posts: [...state.posts, action.payload]
+            };
         case PostActionTypes.REMOVE:
-            return [...state.splice(state.findIndex(p => p.id === action.payload), 1)];
+            return {
+                posts: [...state.posts.splice(
+                    state.posts.findIndex(p => p.id === action.payload), 1)
+                ]
+            };
         case PostActionTypes.UPDATE:
-            return [...state.splice(state.findIndex(p => p.id === action.payload.id), 1), action.payload];
+            return {
+                posts: [...state.posts.splice(
+                    state.posts.findIndex(p => p.id === action.payload.id), 1), action.payload]
+            };
         default:
             return state;
     }
