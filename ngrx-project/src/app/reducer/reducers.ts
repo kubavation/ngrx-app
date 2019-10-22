f03ca71284a63f316490e5cf7fa8bae9a6dccfc2
kubavation <kubavation@wp.pl>
2019-10-22 18:17:30 +0200

    g

--- ngrx-project/src/app/reducer/reducers.ts
+++ ngrx-project/src/app/reducer/reducers.ts
@@ -1,6 +1,8 @@
+import { State } from './state';
 import { PostReducer } from './post.reducer';
 
 export const reducers = {
     posts: PostReducer
 };
 
+export const getPosts = (state: State) => state.posts;
