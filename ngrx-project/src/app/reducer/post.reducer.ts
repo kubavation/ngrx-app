c7b841f74a531f9bda878d8e1ace69a18bac9619
kubavation <kubavation@wp.pl>
2019-10-23 21:38:17 +0200

    fix in getPost selector

--- ngrx-project/src/app/reducer/post.reducer.ts
+++ ngrx-project/src/app/reducer/post.reducer.ts
@@ -1,4 +1,4 @@
-import { State } from './state';
+import { State, PostState } from './state';
 import { ActionParent } from './../actions/action.parent';
 import { PostActionTypes } from './../shared/enum/PostActionTypes.enum';
 import { Post } from './../model/Post';
@@ -13,47 +13,47 @@ export const initialState: State = {
     }
 };
 
-export function PostReducer(state = initialState, action: ActionParent) {
+export function PostReducer(state = initialState.postState, action: ActionParent): PostState {
 
-    //const  = 
+    
     console.log('state')
     console.log(state)
 
     switch (action.type) {
-        case PostActionTypes.ADD:
-            return {
-               // posts: [...state.posts, action.payload]
-            };
-        case PostActionTypes.REMOVE:
-            return {
-                // posts: [...state.posts.splice(
-                //     state.posts.findIndex(p => p.id === action.payload), 1)
-                // ]
-            };
-        case PostActionTypes.UPDATE:
-            return {
-                // posts: [...state.posts.splice(
-                //     state.posts.findIndex(p => p.id === action.payload.id), 1), action.payload]
-            };
-        case PostActionTypes.FETCH:
-            console.log('in fetch')
-            //return state.posts;
-            return {
-                //posts: [...state.posts]
-            };
-        case PostActionTypes.SEARCH:
-            console.log('in search');
-            return {
-                //posts: [...state.posts]
-            };
-        case PostActionTypes.UPDATED:
-            return {
-                posts: [...action.payload]
-            };
+        // case PostActionTypes.ADD:
+        //     return {
+        //        // posts: [...state.posts, action.payload]
+        //     };
+        // case PostActionTypes.REMOVE:
+        //     return {
+        //         // posts: [...state.posts.splice(
+        //         //     state.posts.findIndex(p => p.id === action.payload), 1)
+        //         // ]
+        //     };
+        // case PostActionTypes.UPDATE:
+        //     return {
+        //         // posts: [...state.posts.splice(
+        //         //     state.posts.findIndex(p => p.id === action.payload.id), 1), action.payload]
+        //     };
+        // case PostActionTypes.FETCH:
+        //     console.log('in fetch')
+        //     //return state.posts;
+        //     return {
+        //         //posts: [...state.posts]
+        //     };
+        // case PostActionTypes.SEARCH:
+        //     console.log('in search');
+        //     return {
+        //         //posts: [...state.posts]
+        //     };
+        // case PostActionTypes.UPDATED:
+        //     return {
+        //         posts: [...action.payload]
+        //     };
         default:
             console.log('in default')
             return {
-                posts: []//[...state.posts]
+                    posts: []
             };
     }
 }
--- ngrx-project/src/app/reducer/state.ts
+++ ngrx-project/src/app/reducer/state.ts
@@ -1,4 +1,7 @@
+import { PostState } from './state';
 import { Post } from 'src/app/model/Post';
+import { createSelector } from '@ngrx/store';
+ 
 
 
 export interface PostState {
@@ -11,4 +14,6 @@ export interface State {
 }
 
 
-export const getPosts = (state: State) => { console.log(state); return [];} //todo refactor multiple reducers
+export const selectPostState = (state: State) => state.postState;
+export const getPosts = createSelector(selectPostState, (state) => { console.log(state); return state.posts });
+//export const getPosts = (state: PostState) => { console.log(state); return [];} //todo refactor multiple reducers
